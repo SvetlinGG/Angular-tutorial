313875f2a1b60bf6f69ff3226e0e6639fb45cc42
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-05-11 22:45:42 +0200

    two way data binding - another aprouch

--- first-angular-application/src/app/app.component.ts
+++ first-angular-application/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { FormsModule} from '@angular/forms'
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, HeaderComponent],
+  imports: [RouterOutlet, NavbarComponent, HeaderComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
