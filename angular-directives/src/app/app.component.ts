57117c2036bb98938df377ec3dd382a070c9b675
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-05-12 22:14:38 +0200

    use *ngIf directive

--- angular-directives/src/app/app.component.ts
+++ angular-directives/src/app/app.component.ts
@@ -1,12 +1,16 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-directives';
+  
+  isLoggedIn: boolean = true;
+  userName: string = 'John Doe'
 }
