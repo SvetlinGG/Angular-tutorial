ca0cd1989a49533461ba2d0185585bcbdd818fe0
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-05-13 11:14:44 +0200

    use ng-template

--- angular-directives/src/app/app.component.ts
+++ angular-directives/src/app/app.component.ts
@@ -19,6 +19,8 @@ export class AppComponent {
   isMember: boolean = false;
   isGuest: boolean = true;
 
+  userRole: string = 'Admin'
+
   secondUserName: string = 'Svetlin Garabedyan';
 
   users = [
@@ -31,5 +33,7 @@ export class AppComponent {
 
   increment(){
     this.countNumbers ++;
+    //console.log(this.countNumbers);
+    
   }
 }
